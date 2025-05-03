2f021c92246a5dc4b01c6d809088412a12324484
ManasGeedkar <geedkarmanas@gmail.com>
2025-05-03 06:05:37 +0000

    configuration config done

---
+++ number_math.js
@@ -0,0 +1,39 @@
+
+/*const score = 400
+console.log(score);
+
+const balnace = new Number(400)
+console.log(balnace);
+
+console.log(balnace.toString().length);
+
+const number = 69.8777
+
+console.log(number.toPrecision(2));
+
+
+const hundred = 1000000
+
+console.log(hundred.toLocaleString('en-In'));
+
+//++++++++++++++++++++++++++++++++++++ maths +++++++++++++++++++  */
+
+console.log(Math);
+
+/*console.log(Math.round(4.7));     values 
+
+console.log(Math.ceil(4.7)); 
+
+console.log(Math.abs(-4));
+
+console.log(Math.sqrt(4.7));
+
+console.log(Math.floor(4.7)); 
+
+console.log(Math.max(22,4,5,7)); */
+
+const min = 10
+
+const max = 20
+
+console.log(Math.floor(Math.random () * (max - min + 1)) + min);
