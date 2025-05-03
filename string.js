1d8079465b38fe9ce966ac8a1ec2f1e1f4560733
ManasGeedkar <geedkarmanas@gmail.com>
2025-05-03 05:17:33 +0000

    configuration config done

---
+++ string.js
@@ -0,0 +1,23 @@
+/*const name = "Manas"
+
+const count = 50 
+
+console.log(`My  name is ${name} this is the count ${count} `);
+
+*/ //++++++++++++++++++++++++++++++++++++++++++
+
+const gameName = new String ('manas')
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+
+console.log(gameName.indexOf('a'));  */
+
+const url =  " I am studying in 9th class"
+
+console.log(url.replace("9th" , "12th"));
+
+console.log(url.includes("studing"));
+
+console.log(url.split(" "));  
